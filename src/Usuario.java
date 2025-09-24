e34e3518559bdbe5d4cb22f965bd36bd36675ee7
Gawlz4322 <roberttries0411@gmail.com>
2025-09-24 20:29:16 -0300

    Setter clase usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,11 @@
 public class Usuario {
+    private String username;
+    private String password;
+    private String nombre;
+
+    public Usuario(String username, String password, String nombre) {
+        this.username = username;
+        this.password = password;
+        this.nombre = nombre;
+    }
 }
