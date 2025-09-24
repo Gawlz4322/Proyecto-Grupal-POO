4e7b87720c2ae3dde94162cbdf9597c8be3f4776
Gawlz4322 <roberttries0411@gmail.com>
2025-09-24 20:32:56 -0300

    metodo para inicializar ventana login

--- src/VentanaLogin.java
+++ src/VentanaLogin.java
@@ -4,5 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VentanaLogin {
+    public static final List<Usuario> USUARIOS = new ArrayList<>();
 
+    public VentanaLogin() {
+        //llamar metodos
+    }
 }
