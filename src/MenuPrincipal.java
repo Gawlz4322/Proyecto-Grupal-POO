aee508f4af0f6ed092bdc75da5d1fffa0828b58d
Gawlz4322 <roberttries0411@gmail.com>
2025-09-25 20:53:46 -0300

    Se crea metodo iniciador de componentes del menu principal

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -7,14 +7,18 @@ import java.util.List;
 public class MenuPrincipal {
     //menu principal, se inicializa después de login
     private final JFrame frame = new JFrame("Menú Principal - CFP");
+
     public MenuPrincipal() {
         //aun no se crean los métodos.
         iniciarVentanaMenuPrincipal();
     }
+
     private void iniciarVentanaMenuPrincipal() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLayout(null);
     }
+    private void iniciarComponentes() {
 
+    }
 }
