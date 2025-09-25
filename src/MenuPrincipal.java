8f1aca07b0f545eb60d65759fa0cfbd70cbf7f21
Gawlz4322 <roberttries0411@gmail.com>
2025-09-25 20:41:16 -0300

    Se crea metodo de preguntas iniciales apenas cierra el login

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -1,4 +1,19 @@
+import javax.swing.*;
+
 public class MenuPrincipal {
     //menu principal, se inicializa después de login
 
+    //preguntas iniciales, aparecen apenas cierra login y abre menu. Historial y display de dinero actual todavia no implementados
+    public void preguntasInicialesMenu() {
+        int gastos = JOptionPane.showConfirmDialog(null, "¿Has realizado gastos?", "Gastos", JOptionPane.YES_NO_OPTION);
+        if (gastos == JOptionPane.YES_OPTION) {
+            String montoGastos = JOptionPane.showInputDialog(null, "Ingrese el monto de gastos:");
+        }
+
+        int ingresos = JOptionPane.showConfirmDialog(null, "¿Has recibido ingresos?", "Ingresos", JOptionPane.YES_NO_OPTION);
+        if (ingresos == JOptionPane.YES_OPTION) {
+            String montoIngresos = JOptionPane.showInputDialog(null, "Ingrese el monto de ingresos:");
+        }
+
+    }
 }
