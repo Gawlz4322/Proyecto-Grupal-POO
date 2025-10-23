9ca9b5ce5fd5081b151e7484eedc1efebc0f2107
Gawlz4322 <roberttries0411@gmail.com>
2025-10-22 23:28:49 -0300

    Constructor inicial

--- src/Controlador/LoginController.java
+++ src/Controlador/LoginController.java
@@ -1,4 +1,10 @@
 package Controlador;
 
+import Modelo.SistemaFinanzas;
+
 public class LoginController {
+    private final SistemaFinanzas modelo;
+    public LoginController(SistemaFinanzas modelo){
+        this.modelo = modelo;
+    }
 }
