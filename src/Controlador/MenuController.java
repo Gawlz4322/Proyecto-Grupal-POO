d93cda7e4b78b89cfe892d27d9b1fafcebb48397
Gawlz4322 <roberttries0411@gmail.com>
2025-10-23 00:09:30 -0300

    Se crea clase controlador inicial, se importan clases que se van a ocupar

--- src/Controlador/MenuController.java
+++ src/Controlador/MenuController.java
@@ -1,4 +1,13 @@
 package Controlador;
 
+import Modelo.SistemaFinanzas;
+import Vista.MenuPrincipal;
+
 public class MenuController {
+    private MenuPrincipal Vista;
+    private final SistemaFinanzas Modelo;
+
+    public MenuController(SistemaFinanzas modelo) {
+        this.Modelo = modelo;
+    }
 }
