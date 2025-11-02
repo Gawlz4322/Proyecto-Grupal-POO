1e8b1c0bd39680a368547e4ee6ca9685d6c27af9
Gawlz4322 <roberttries0411@gmail.com>
2025-11-02 19:47:41 -0300

    Corrección de código. Innecesario con el nuevo registro.

--- src/Modelo/SistemaFinanzas.java
+++ src/Modelo/SistemaFinanzas.java
@@ -4,16 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SistemaFinanzas {
-    public static final List<Usuario> USUARIOS = new ArrayList<>();
     private double saldo = 0;
     private final List<String> historial = new ArrayList<>();
 
     public SistemaFinanzas() {
-        inicializarUsuarios();
-    }
-    public void inicializarUsuarios(){
-        USUARIOS.add(new Usuario("admin", "1234", "Administrador"));
-        USUARIOS.add(new Usuario("Juanin", "abcd", "Juanin Juan Harry"));
+
     }
 
     public void agregarGasto(double monto) {
